fb509c6527fcdf5c06dd9baf090e4c97c7b2c573
Marcel Mulders <marcel.mulders@hotmail.com>
2022-03-21 12:34:39 +0000

    Created element tests for App.jsx, leftContainer.jsx, rightContainer.jsx

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,34 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
+import LeftContainer from './components/timelineContainers/leftContainer';
+import RightContainer from './components/timelineContainers/rightContainer';
 
-test('renders learn react link', () => {
+test('App element', () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+  const element = screen.getByText(/Event View/i);
+  expect(element).toBeInTheDocument();
+});
+
+test('Left container element', () => {
+  render(
+    <LeftContainer
+      title='I am a left container'
+      time='13:00'
+      description='I am a description'
+    />
+  );
+  const element = screen.getByText(/I am a left container/i);
+  expect(element).toBeInTheDocument();
+});
+
+test('Right container element', () => {
+  render(
+    <RightContainer
+      title='I am a right container'
+      time='13:00'
+      description='I am a description'
+    />
+  );
+  const element = screen.getByText(/I am a right container/i);
+  expect(element).toBeInTheDocument();
 });
